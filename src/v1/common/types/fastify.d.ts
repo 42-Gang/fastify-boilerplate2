6429c4a5a5d5652b0ef7060226712897bdee72cf
kungbi <kipper12335@gmail.com>
2025-04-01 15:56:16 +0900

    feat(fastify): FastifyRequest에 myId 추가

--- src/v1/common/types/fastify.d.ts
+++ src/v1/common/types/fastify.d.ts
@@ -1,16 +1,12 @@
 import 'fastify';
 
-import { User } from '@prisma/client';
-import { JWT } from '@fastify/jwt';
-
 declare module 'fastify' {
   interface FastifyInstance {
     authenticate: (request: FastifyRequest, reply: FastifyReply) => Promise<void>;
   }
 
   interface FastifyRequest {
-    jwt: JWT;
     authorized: boolean;
-    me: User;
+    myId: number;
   }
 }
